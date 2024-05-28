e9a9759122760c89d3c44e583c1dbee5ad3e76ad
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-28 10:48:12 +0000

    sidebar implemented

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,3 +1,83 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { SIDEBAR_LINKS } from "@/constants";
+import { cn } from "@/lib/utils";
+
 export const MobileNav = () => {
-  return <section className="w-full max-w-[264px]">MobileNav</section>;
+  const pathname = usePathname();
+
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Image
+            src="/icons/hamburger.svg"
+            alt="Open sidebar"
+            width={36}
+            height={36}
+            className="cursor-pointer sm:hidden"
+          />
+        </SheetTrigger>
+
+        <SheetContent side="left" className="border-none bg-dark-1">
+          <SheetClose asChild>
+            <Link href="/" className="flex items-center gap-1">
+              <Image
+                src="/icons/logo.svg"
+                alt="Yoom logo"
+                width={32}
+                height={32}
+                className="max-sm:size-10"
+              />
+
+              <p className="text-[26px] font-extrabold text-white">Yoom</p>
+            </Link>
+          </SheetClose>
+
+          <div className="flex h-[calc(100vh_-_72px)] flex-col justify-between overflow-y-auto">
+            <section className="flex flex-col h-full gap-6 pt-16 text-white">
+              {SIDEBAR_LINKS.map((item) => {
+                const isActive =
+                  pathname === item.route ||
+                  pathname.startsWith(`${item.route}/`);
+
+                return (
+                  <SheetClose key={item.route} asChild>
+                    <Link
+                      href={item.route}
+                      className={cn(
+                        "flex gap-4 items-center p-4 rounded-lg w-full max-w-60",
+                        {
+                          "bg-blue-1": isActive,
+                        }
+                      )}
+                    >
+                      <Image
+                        src={item.imgUrl}
+                        alt={item.label}
+                        width={20}
+                        height={20}
+                      />
+
+                      <p className="font-semibold">{item.label}</p>
+                    </Link>
+                  </SheetClose>
+                );
+              })}
+            </section>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
 };
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { SIDEBAR_LINKS } from "@/constants";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 
 export const Sidebar = () => {
   const pathname = usePathname();
@@ -14,7 +14,8 @@ export const Sidebar = () => {
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {SIDEBAR_LINKS.map((item) => {
-          const isActive = pathname === item.route;
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
 
           return (
             <Link
