24ee7cf5bac0c00ceb98650eee8f5a842f07c219
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-28 10:18:10 +0000

    sidebar links constants added

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const SIDEBAR_LINKS = [
+  {
+    label: "Home",
+    route: "/",
+    imgUrl: "/icons/home.svg",
+  },
+  {
+    label: "Upcoming",
+    route: "/upcoming",
+    imgUrl: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imgUrl: "/icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imgUrl: "/icons/video.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imgUrl: "/icons/add-personal.svg",
+  },
+] as const;
