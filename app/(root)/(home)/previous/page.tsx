7280e59f3e9a89d4cb760d4b3ddd3b8ce2090462
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-05-29 12:29:50 +0000

    previous recording page completed

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,7 +1,11 @@
+import { CallList } from "@/components/call-list";
+
 const PreviousPage = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Previous</h1>
+
+      <CallList type="ended" />
     </section>
   );
 };
